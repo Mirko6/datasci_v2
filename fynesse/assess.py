de73516f84109435a7e457756bcbb5fbf97c47d7
Miro Macko <miro.macko@nozdormu.net>
2022-11-26 17:52:07 +0000

    filter outliers + plot price distribution

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,6 +1,9 @@
 from .config import *
 
 from . import access_db
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 """These are the types of import we might expect in this file
 import pandas
@@ -29,3 +32,37 @@ def view(data):
 def labelled(data):
     """Provide a labelled set of data ready for supervised learning."""
     raise NotImplementedError
+
+
+def filter_price_outliers(df, fraction_to_remove = 0.1):
+  print(f"Number of all houses: {len(df)}, price range: {df['price'].min()} - {df['price'].max()}")
+  one_sided_outliers = fraction_to_remove / 2
+  low_price = df['price'].quantile(one_sided_outliers, interpolation='nearest')
+  high_price = df['price'].quantile(1 - one_sided_outliers, interpolation='nearest')
+  df_filtered = df[
+      (low_price <= df['price']) &
+      (df['price'] <= high_price)
+  ]
+  print(f"Number of houses without outliers: {len(df_filtered)}, price range: {df_filtered['price'].min()} - {df_filtered['price'].max()}")
+  return df_filtered
+
+
+def plot_price_distribution(prices, bin_width = 100_000):
+  bin_low = math.floor(prices.min() / bin_width) * bin_width
+  bin_high = math.ceil(prices.max() / bin_width) * bin_width
+
+  bins = np.linspace(bin_low, bin_high, num = (bin_high - bin_low) // bin_width + 1)
+  
+  n, bins, patches = plt.hist(prices, bins=bins)
+
+  #cmap scaling
+  bin_centers = 0.5 * (bins[:-1] + bins[1:])
+  col = bin_centers - min(bin_centers)
+  col /= max(col)
+  color_map = plt.cm.get_cmap('plasma')
+  for c, p in zip(col, patches):
+    plt.setp(p, 'facecolor', color_map(c))
+
+  plt.ylabel('Count')
+  plt.xlabel('Price')
+  plt.title('Counts per price_level')
