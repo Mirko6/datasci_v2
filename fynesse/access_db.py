970f0cd5159f8477bb10cbf2ad9c941ca9bc509b
Miro Macko <miro.macko@nozdormu.net>
2022-12-01 14:49:14 +0000

    add documentation

--- fynesse/access_db.py
+++ fynesse/access_db.py
@@ -57,17 +57,34 @@ def create_connection(user, password, host, database, port=3306):
 
 
 class DB:
+  """
+  Class designed to simplify execution of queries on the database.
+
+  Methods
+  -------
+  __init__(conn):
+    create a DB instance using a given connection
+
+  custom_query(query):
+    execute arbitrary SQL syntax query and return the result
+
+  custom_select_query(query):
+    execute arbitrary SELECT SQL syntax query and return 
+    the result as a DataFrame
+
+  select_top(table_name, n):
+    Select top n elementas from table table_name and return
+    the result as a DataFrame
+
+  select_priced_paid_data_joined_on_postcode(**kwargs):
+    Join and Select priced paid data table with postcode data allowing for many filters
+  """
   def __init__(self, conn: Connection) -> None:
      self.conn = conn
      self.conn.ping()
      current_db = self.custom_query("SELECT database()")[0][0]
      tables = (table[0] for table in self.custom_query("SHOW TABLES"))
      print(f"you now have access to database: {current_db} with tables: {', '.join(tables)}")
-
-
-  def select_top(self, table_name: str,  n: int = 5) -> pd.DataFrame:
-    query = f'SELECT * FROM {table_name} LIMIT {n}'
-    return self.custom_select_query(query)
 
 
   def custom_query(self, query: str):
@@ -85,6 +102,11 @@ class DB:
     return pd.DataFrame.from_records(rows, columns=column_names)
 
 
+  def select_top(self, table_name: str,  n: int = 5) -> pd.DataFrame:
+    query = f'SELECT * FROM {table_name} LIMIT {n}'
+    return self.custom_select_query(query)
+
+
   def select_priced_paid_data_joined_on_postcode(
       self,
       table_name_priced_paid_data: str = "pp_data",
@@ -99,7 +121,28 @@ class DB:
       only_live_postcodes : bool = False, # it is okay to include transactions associated with terminated postcodes
       property_type: Optional[str] = None,
       ppd_category_type: Optional[str] = 'A',
-    ):
+      columns_to_select: List[str] = [
+        "price", "date_of_transfer", "property_type", "tenure_type", "new_build_flag", "country", "county", "town_city", "district", "longitude", "lattitude", "postcode_status", "ppd_category_type"
+      ]
+    ) -> pd.DataFrame:
+      """Select Join and Select priced paid data table with postcode data allowing for many filters
+
+      Args:
+          table_name_priced_paid_data (str, optional): name of the priced paid data table. Defaults to "pp_data".
+          table_name_postcode_data (str, optional): name of the postcode data table.. Defaults to "postcode_data".
+          date_from_incl (Optional[str], optional): lower bound filter on date. Defaults to None.
+          postcode (Optional[str], optional): postcode filter. Defaults to None.
+          longitude (Optional[str], optional): longitude coordinate of the center of a box. Defaults to None.
+          lattitude (Optional[str], optional): lattitude coordinate of the center of a box. Defaults to None.
+          box_size (int, optional): this will create filter for transactions ensuring they happened in a square of size box_size with centre in (longitude, lattitude). 
+            Defaults to 0.01. Which corresponds to 1.1km.
+          ppd_category_type (Optional[str], optional): ppd_category_type filter. Defaults to 'A'.
+          columns_to_select (List[str]): Columns to select in addition to postcode. 
+            Defaults to [price, date_of_transfer, property_type, tenure_type, new_build_flag, country, county, town_city, district, longitude, lattitude, postcode_status, ppd_category_type]
+
+      Returns:
+          DataFrame with corresponding data
+      """      
       filters = []
       if date_from_incl is not None:
         filters.append(f"'{date_from_incl}' <= date_of_transfer")
@@ -121,75 +164,14 @@ class DB:
         filters.append(f"property_type = '{property_type}'")
       if ppd_category_type is not None:
         filters.append(f"ppd_category_type = '{ppd_category_type}'")
-      
+
+      columns_to_select.append(f"{table_name_priced_paid_data}.postcode")
       query = f"""
-        SELECT price, date_of_transfer, property_type, tenure_type, new_build_flag, 
-               country, county, town_city, district, longitude, lattitude, 
-               {table_name_priced_paid_data}.postcode, status AS postcode_status,
-               ppd_category_type
+        SELECT {', '.join(columns_to_select)}
           FROM {table_name_priced_paid_data}
           JOIN {table_name_postcode_data} 
             ON {table_name_priced_paid_data}.postcode = {table_name_postcode_data}.postcode
          WHERE {' AND '.join(filters)}
       """
       print(f"the following query gets executed: {query}")
-      return self.custom_select_query(query)
-
-  #not really faster
-  def select_priced_paid_data_joined_on_postcode_faster_on_coordinates(
-      self,
-      longitude: str,
-      lattitude: str,
-      table_name_priced_paid_data: str = "pp_data",
-      table_name_postcode_data: str = "postcode_data",
-      date_from_incl: Optional[str] = None, #yyyy/mm/dd
-      date_to_excl: Optional[str] = None, #yyyy/mm/dd
-      town_city: Optional[str] = None,
-      postcode: Optional[str] = None,
-      box_size: int = 0.01, #0.01 almost equals to 1.1km
-      only_live_postcodes : bool = False, # it is okay to include transactions associated with terminated postcodes
-      property_type: Optional[str] = None,
-      ppd_category_type: Optional[str] = 'A',
-    ):
-      filters = []
-      if date_from_incl is not None:
-        filters.append(f"'{date_from_incl}' <= date_of_transfer")
-      if date_to_excl is not None:
-        filters.append(f"date_of_transfer < '{date_to_excl}'")
-      if town_city is not None:
-        filters.append(f"town_city = '{town_city.upper()}'")
-      if postcode is not None:
-        filters.append(f"{table_name_priced_paid_data}.postcode = '{postcode}'")
-      if only_live_postcodes:
-        filters.append("status = 'live'")
-      if property_type is not None:
-        filters.append(f"property_type = '{property_type}'")
-      if ppd_category_type is not None:
-        filters.append(f"ppd_category_type = '{ppd_category_type}'")
-      
-      longitude = float(longitude)
-      lattitude = float(lattitude)
-      select_applicable_postcodes = f"""
-        SELECT postcode 
-          FROM {table_name_postcode_data}
-         WHERE {longitude - box_size/2} < longitude AND
-               longitude < {longitude + box_size/2} AND
-               {lattitude - box_size/2} < lattitude AND
-               lattitude < {lattitude + box_size/2}
-      """
-      filters.append(
-        f"{table_name_priced_paid_data}.postcode IN ({select_applicable_postcodes})"
-      )
-
-      query = f"""
-        SELECT price, date_of_transfer, property_type, tenure_type, new_build_flag, 
-               country, county, town_city, district, longitude, lattitude, 
-               {table_name_priced_paid_data}.postcode, status AS postcode_status,
-               ppd_category_type
-          FROM {table_name_priced_paid_data}
-          JOIN {table_name_postcode_data} 
-            ON {table_name_priced_paid_data}.postcode = {table_name_postcode_data}.postcode
-         WHERE {' AND '.join(filters)}
-      """
-      print(query)
       return self.custom_select_query(query)
